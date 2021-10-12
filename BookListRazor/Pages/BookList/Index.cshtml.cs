43793af6288f9421a4e9b084a3ecccef345641c7
Himang Patel <tbs.Himang@gmail.com>
2021-10-12 16:40:48 +0530

    First commit

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
